09c232397f7999964d0e199c37038712619f3d9e
Viktoriia <pecourses@gmail.com>
2021-08-15 15:42:31 +0300

    Add mapDispatchToProps

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,37 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ACTION_TYPES from '../../actions/actionTypes';
+// import ACTION_TYPES from '../../actions/actionTypes';
 import * as actionCreators from './../../actions';
 
 function Counter (props) {
-  const { count, step, dispatch } = props;
-
-  const increment = () => {
-    dispatch(actionCreators.increment());
-  };
-
-  const decrement = () => {
-    dispatch(actionCreators.decrement());
-  };
+  const {
+    count,
+    step,
+    incrementAction,
+    decrementAction,
+    setStepAction,
+  } = props;
 
   const setStep = event => {
-    dispatch(actionCreators.setStep(Number(event.target.value)));
+    setStepAction(Number(event.target.value));
   };
 
   return (
     <article>
       <h1>Count: {count}</h1>
       <input type='number' value={step} onChange={setStep} />
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
+      <button onClick={decrementAction}>-</button>
+      <button onClick={incrementAction}>+</button>
     </article>
   );
 }
 
 const mapStateToProps = state => state;
 
+const mapDispatchToProps = dispatch => {
+  return {
+    incrementAction: () => {
+      dispatch(actionCreators.increment());
+    },
+    decrementAction: () => {
+      dispatch(actionCreators.decrement());
+    },
+    setStepAction: value => {
+      dispatch(actionCreators.setStep(value));
+    },
+  };
+};
+
 // const withState = connect(mapStateToProps);
 // const CounterWithState = withState(Counter);
 // export default CounterWithState;
 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
