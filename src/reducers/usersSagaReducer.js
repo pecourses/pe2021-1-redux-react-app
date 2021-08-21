9e97db54ffb72c4eeb3fb0bb3d816923678657f8
Viktoriia <pecourses@gmail.com>
2021-08-21 18:28:35 +0300

    Add userReducer

---
+++ src/reducers/usersSagaReducer.js
@@ -0,0 +1,38 @@
+import ACTION_TYPES from './../actions/actionTypes';
+
+const initialState = {
+  users: [],
+  isFetching: false,
+  error: null,
+};
+
+function usersSagaReducer (state, action) {
+  const { type } = action;
+  switch (type) {
+    case ACTION_TYPES.GET_USERS_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case ACTION_TYPES.GET_USERS_SUCCESS: {
+      const { users } = action;
+      return {
+        ...state,
+        isFetching: false,
+        users: users,
+      };
+    }
+    // ACTION_TYPES.GET_USERS_ERROR
+    case ACTION_TYPES.GET_USERS_ERROR: {
+      const { error } = action;
+      return {
+        ...state,
+        isFetching: false,
+        error: error,
+      };
+    }
+    default:
+      return state;
+  }
+}
