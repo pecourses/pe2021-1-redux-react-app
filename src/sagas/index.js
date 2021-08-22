d48c20d7883999c1aac667f106b5a98bc1e9cee7
Viktoriia <pecourses@gmail.com>
2021-08-22 17:02:47 +0300

    Add user saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,10 @@
 import { takeLatest } from 'redux-saga/effects';
 import ACTION_TYPES from '../actions/actionTypes';
-import { getUsersSaga } from './usersSaga';
+import { createUserSaga, getUsersSaga } from './usersSagas';
 
 function * rootSaga () {
   yield takeLatest(ACTION_TYPES.GET_USERS_ACTION, getUsersSaga);
+  yield takeLatest(ACTION_TYPES.CREATE_USER_ACTION, createUserSaga);
 }
 
 export default rootSaga;
---
+++ src/sagas/usersSagas.js
@@ -0,0 +1,43 @@
+import { put } from 'redux-saga/effects';
+import {
+  createUserError,
+  createUserRequest,
+  createUserSuccess,
+  getUsersError,
+  getUsersRequest,
+  getUsersSuccess,
+} from '../actions';
+import * as API from '../api';
+
+export function * getUsersSaga () {
+  // отправить экшн ACTION_TYPES.GET_USERS_REQUEST
+  yield put(getUsersRequest()); // отправка екшна
+  // yield put({ type: ACTION_TYPES.GET_USERS_REQUEST }); // отправка екшна
+
+  try {
+    //   юзеры = загрузка()
+    const { data: users } = yield API.getUsers();
+    //   отправить экшн  ACTION_TYPES.GET_USERS_SUCCESS
+
+    yield put(getUsersSuccess(users));
+    // yield put({
+    //   type: ACTION_TYPES.GET_USERS_SUCCESS,
+    //   users: users,
+    // });
+  } catch (e) {
+    //    отправить экшн  ACTION_TYPES.GET_USERS_ERROR
+    yield put(getUsersError(e));
+  }
+}
+
+export function * createUserSaga (action) {
+  const { user } = action;
+
+  yield put(createUserRequest());
+  try {
+    const { data: newUser } = yield API.createUser(user);
+    yield put(createUserSuccess(newUser));
+  } catch (e) {
+    yield put(createUserError(e));
+  }
+}
